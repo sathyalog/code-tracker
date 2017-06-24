c24cf803030c9ca027dd2f69fc634757a5fc6ee8
svakacharla@sapient.com <svakacharla@sapient.com>
2017-06-24 22:25:54 +0100

    configured firebase and enabled google signin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import {Http } from '@angular/http';
-
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+import {Observable} from 'rxjs/Observable';
+import * as firebase from 'firebase/app';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,4 +11,17 @@ import {Http } from '@angular/http';
 })
 export class AppComponent {
   title = 'Welcome to Helios Tracker';
+  user: Observable<firebase.User>;
+
+  constructor(public afAuth: AngularFireAuth) {
+    this.user = afAuth.authState;
+  }
+
+  login() {
+    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  }
+
+  logout() {
+    this.afAuth.auth.signOut();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,21 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MdButtonModule, MdCardModule, MdMenuModule, MdToolbarModule, MdIconModule, MdCheckboxModule, MdDatepickerModule, MdInputModule, MdRadioModule, MdSelectModule, MdSlideToggleModule } from '@angular/material';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireModule } from 'angularfire2';
+
+export const firebaseConfig = {
+
+    apiKey: "AIzaSyC1285iSDRdLc_GLM3uj0C4rvH5Bg7llDU",
+    authDomain: "code-tracker-30fb9.firebaseapp.com",
+    databaseURL: "https://code-tracker-30fb9.firebaseio.com",
+    projectId: "code-tracker-30fb9",
+    storageBucket: "code-tracker-30fb9.appspot.com",
+    messagingSenderId: "761999403716"
+ 
+ };
+
 
 @NgModule({
   declarations: [
@@ -26,7 +41,10 @@ import { MdButtonModule, MdCardModule, MdMenuModule, MdToolbarModule, MdIconModu
     MdInputModule,
     MdRadioModule,
     MdSelectModule,
-    MdSlideToggleModule
+    MdSlideToggleModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
 
   ],
   providers: [],
